d21a2aec8eec5b5def20021b0d052ee2decbca97
Govarthan Anandhan <86225331+govaanand37@users.noreply.github.com>
2024-04-11 15:22:54 +0530

    Added appspeca and depend

---
+++ scripts/start_container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+# Install docker
+sudo apt install docker.io -y
+
+# Pull the Docker image from Docker Hub
+docker pull govarthan/simple-python-flask-app:latest
+
+# list Docker images
+docker images
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 govarthan/simple-python-flask-app:latest
